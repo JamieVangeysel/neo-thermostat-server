38d2997c416ebf9a10a239d3d617ff46f1a4c250
Jamie Vangeysel <jamie.vangeysel@simplintho.com>
2020-10-23 07:01:48 +0200

    correct HI measurement

--- src/services/thermostat.ts
+++ src/services/thermostat.ts
@@ -215,13 +215,12 @@ export class Thermostat {
     let HI = 0.5 * (T + 61.0 + ((T - 68.0) * 1.2) + (RH * 0.094));
 
     if ((HI + T) / 2 >= 80) {
+      // tslint:disable-next-line: max-line-length
+      HI = -42.379 + 2.04901523 * T + 10.14333127 * RH - .22475541 * T * RH - .00683783 * T * T - .05481717 * RH * RH + .00122874 * T * T * RH + .00085282 * T * RH * RH - .00000199 * T * T * RH * RH;
+
       if (T >= 80 && T <= 112 && RH <= 13) {
-        // tslint:disable-next-line: max-line-length
-        HI = -42.379 + 2.04901523 * T + 10.14333127 * RH - .22475541 * T * RH - .00683783 * T * T - .05481717 * RH * RH + .00122874 * T * T * RH + .00085282 * T * RH * RH - .00000199 * T * T * RH * RH;
         ADJUSTMENT = -1 * (((13 - RH) / 4) * Math.sqrt((17 - Math.abs(T - 95.)) / 17));
       } else if (T >= 80 && T <= 87 && RH >= 85) {
-        // tslint:disable-next-line: max-line-length
-        HI = -42.379 + 2.04901523 * T + 10.14333127 * RH - .22475541 * T * RH - .00683783 * T * T - .05481717 * RH * RH + .00122874 * T * T * RH + .00085282 * T * RH * RH - .00000199 * T * T * RH * RH;
         ADJUSTMENT = ((RH - 85) / 10) * ((87 - T) / 5);
       }
     }
