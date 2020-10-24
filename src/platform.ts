83bc5383d5739d9cdf2b244beee5aa17b67d5741
Jamie Vangeysel <jamie.vangeysel@simplintho.com>
2020-10-24 13:30:39 +0200

    add database service to platform

--- src/platform.ts
+++ src/platform.ts
@@ -2,12 +2,14 @@ import { HttpListener } from './services/http-listener';
 import { Logger } from './services/logging/logger';
 import { Thermostat } from './services/thermostat';
 import { ConfigService, IConfig } from './services/config';
+import { DatabaseService } from './services/database';
 
 export class Platform {
   logger = new Logger();
   config: IConfig;
   configService: ConfigService = new ConfigService(this);
   http: HttpListener = new HttpListener(this);
+  database: DatabaseService;
 
   constructor() {
     this.logger.log(`Platform.constructor() -- start`);
@@ -24,6 +26,8 @@ export class Platform {
       this.logger.log(`Platform.init() -- set config`);
       const thermostat = new Thermostat(this);
       this.logger.log(`Platform.init() -- initialized new Thermostat()`);
+      this.database = new DatabaseService(this);
+      this.logger.log(`Platform.init() -- initialized new DatabaseService()`);
       this.http.configure(config.hostname, config.port, thermostat);
       this.logger.log(`Platform.init() -- configure http instance`);
     });
