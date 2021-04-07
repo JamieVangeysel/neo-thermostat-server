daafc5483e529ebdf54fe34d1a26035688fd00d8
Jamie Vangeysel <jamie.vangeysel@simplintho.com>
2021-04-07 19:31:55 +0200

    remove ;

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { Platform } from './platform';
-let platform: Platform;
+import { Platform } from './platform'
+let platform: Platform
 
 /**
  * @description: this is the entry point of the program, return true if the application started
@@ -7,35 +7,35 @@ let platform: Platform;
 const main = (debug?: boolean): Promise<boolean> => {
   return new Promise<boolean>((resolve, reject) => {
     try {
-      platform = new Platform();
+      const platform = new Platform()
       // created platform
-      resolve(true);
+      resolve(true)
     } catch (err) {
-      reject(err);
+      reject(err)
     }
-  });
-};
+  })
+}
 
-export default main;
+export default main
 
-main();
+main()
 
 // catch ctrl+c event and exit normally
 process.on('SIGINT', async _ => {
-  // await bot.kill();
-  console.debug('SIGINT!');
-  process.exit();
-});
+  // await bot.kill()
+  console.debug('SIGINT!')
+  process.exit()
+})
 
 // catch uncaught exceptions, trace, then restart
 process.on('uncaughtException', err => {
   try {
-    console.debug('process.uncaughtException -- Try to kill main if still running.');
-    // bot.kill();
+    console.debug('process.uncaughtException -- Try to kill main if still running.')
+    // bot.kill()
   } catch {
-    console.error('process.uncaughtException -- There was an error killing main.');
-    // bot.kill();
+    console.error('process.uncaughtException -- There was an error killing main.')
+    // bot.kill()
   }
-  console.error('process.uncaughtException -- Starting new instance of main.', err);
-  // main();
-});
+  console.error('process.uncaughtException -- Starting new instance of main.', err)
+  // main()
+})
