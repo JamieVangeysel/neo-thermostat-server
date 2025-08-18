2ff38786796ae905e5c5e1a5b592a1507e91ae54
Jamie Vangeysel <32323785+JamieVangeysel@users.noreply.github.com>
2025-08-18 12:18:08 +0200

    valve api ready for testing

--- src/services/api/controllers/valvesController.ts
+++ src/services/api/controllers/valvesController.ts
@@ -1,9 +1,11 @@
-import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
+import { FastifyReply, FastifyRequest } from 'fastify'
 import { Platform } from '../../../platform'
 import { IRelaisSwitch } from '../../config'
 
 export default async function valvesController(fastify: any) {
   fastify.get('', handleGetValves)
+  fastify.get('/:id/active', handleGetValveActive)
+  fastify.get('/:id/in-use', handleGetValveInUse)
 }
 
 async function handleGetValves(request: FastifyRequest, reply: FastifyReply) {
@@ -13,10 +15,6 @@ async function handleGetValves(request: FastifyRequest, reply: FastifyReply) {
   let response: FastifyReply
 
   try {
-    // const pool: ConnectionPool = await fastify.getSqlPool(DB_NAME)
-    // const repo = new Neo(request.log, pool)
-
-    // const result = await repo.get(request.params.uuid)
     const allSwitches: IRelaisSwitch[] = platform.config.instances.reduce((prev, curr) => prev.concat(curr.switches), [])
     const valves: IRelaisSwitch[] = allSwitches.filter(e => e.type.indexOf('VALVE') > -1)
 
@@ -39,6 +37,43 @@ async function handleGetValves(request: FastifyRequest, reply: FastifyReply) {
     }
   } catch (err) {
     // platform.logger.error({ err }, 'unknown error', undefined, performance.now() - start)
+    response = reply.error('unknown server error', undefined, performance.now() - start)
+  }
+
+  return response
+}
+
+async function handleGetValveActive(request: FastifyRequest<{ Params: { id: number } }>, reply: FastifyReply) {
+  const platform: Platform = request.locals
+  const id = +request.params.id
+
+  const start = performance.now()
+  let response: FastifyReply
+  try {
+    const allSwitches: IRelaisSwitch[] = platform.config.instances.reduce((prev, curr) => prev.concat(curr.switches), [])
+    const valve: IRelaisSwitch = allSwitches.find(e => e.pinIndex === id)
+    response = reply.success(valve.active)
+  } catch (err) {
+    // platform.logger.error({ err }, 'unknown error', undefined, performance.now() - start)
+    response = reply.error('unknown server error', undefined, performance.now() - start)
+  }
+
+  return response
+}
+
+async function handleGetValveInUse(request: FastifyRequest<{ Params: { id: number } }>, reply: FastifyReply) {
+  const platform: Platform = request.locals
+  const id = +request.params.id
+
+  const start = performance.now()
+  let response: FastifyReply
+  try {
+    const allSwitches: IRelaisSwitch[] = platform.config.instances.reduce((prev, curr) => prev.concat(curr.switches), [])
+    const valve: IRelaisSwitch = allSwitches.find(e => e.pinIndex === id)
+    response = reply.success(valve.active)
+  } catch (err) {
+    // platform.logger.error({ err }, 'unknown error', undefined, performance.now() - start)
+    response = reply.error('unknown server error', undefined, performance.now() - start)
   }
 
   return response
