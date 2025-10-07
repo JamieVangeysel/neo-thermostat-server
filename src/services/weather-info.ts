89c4bd8a07b47a4ecf0bb7f665065a345f8ac1bf
Jamie Vangeysel <32323785+JamieVangeysel@users.noreply.github.com>
2025-10-07 14:12:52 +0200

    update openWeatherInfo implementation (start and update fn) use coordinates instead of city name

--- src/services/weather-info.ts
+++ src/services/weather-info.ts
@@ -11,38 +11,52 @@ export class WeatherInfoService extends EventEmitter {
     this.platform.logger.debug(`WeatherInfoService() -- start`)
     if (platform.config.weatherMapApiKey && platform.config.weatherMapApiKey.trim().length > 0) {
       this.platform.logger.debug(`WeatherInfoService() -- has weatherMapApiKey, start interval`)
-      this.startInterval().then()
+      this.start().then()
     } else {
       this.platform.logger.warn(`WeatherInfoService() -- no weatherMapApiKey configured, skip `)
     }
     this.platform.logger.debug(`WeatherInfoService() -- end`)
   }
 
-  private async startInterval() {
-    this.platform.logger.debug(`WeatherInfoService.startInterval() -- start`)
+  private async start() {
+    this.platform.logger.debug(`WeatherInfoService.start() -- start`)
+    const update = this.update.bind(this)
+    setInterval(update, 300000)
+    await update()
+    this.platform.logger.debug(`WeatherInfoService.start() -- end`)
+  }
 
-    setInterval(async () => {
-      try {
-        const forecast = await this.getForecast('Hasselt,be')
-        this.emit('forecast', forecast)
-      } catch (err) {
-        this.emit('error', err)
-      }
-    }, 300000)
-
+  async update() {
+    this.platform.logger.debug(`WeatherInfoService.update() -- start`)
     try {
-      const forecast = await this.getForecast('Hasselt,be')
+      const forecast = await this.getForecastL(50.89323461766859, 5.31859673594343) // 50.89323461766859, 5.31859673594343
       this.emit('forecast', forecast)
     } catch (err) {
       this.emit('error', err)
     } finally {
-      this.platform.logger.debug(`WeatherInfoService.startInterval() -- end`)
+      this.platform.logger.debug(`WeatherInfoService.start() -- end`)
     }
   }
 
   /**
    * @description query OpenWeatherMapAPi
    * @private
+   * @param {number} lat
+   * @param {number} lon
+   * @return {Promise<OpenWeatherMapResponse>}
+   * @memberof WeatherInfoService
+   */
+  async getForecastL(lat: number, lon: number): Promise<OpenWeatherMapResponse> {
+    this.platform.logger.debug(`WeatherInfoService.getForecast() -- start`, lat, lon)
+    const resp = await fetch(`https://api.openweathermap.org/data/2.5/weather?APPID=${this.platform.config.weatherMapApiKey}&units=metric&lat=${lat}&lon=${lon}`).then(r => r.json())
+    this.platform.logger.debug(`WeatherInfoService.getForecast() -- end`)
+    return await resp
+  }
+
+  /**
+   * @description query OpenWeatherMapAPi
+   * @private
+   * @deprecated
    * @param {string} query example query => Hasselt,be
    * @return {Promise<OpenWeatherMapResponse>}
    * @memberof WeatherInfoService
